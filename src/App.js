714dc10212c1b7d44db0fb356007d3e6eee30cb6
William Elliott <wcelliott1991@gmail.com>
2020-01-03 11:51:08 -0500

    updated menu-items with basic routing navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
+
+const HatsPage = (props) => {
+  console.log("PROPS", props);
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
